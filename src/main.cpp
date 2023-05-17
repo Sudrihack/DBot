889a27cae165a643b5b4a77c9788f4edee7cc405
Baptiste Desfontaines <baptistedesfontaines@pm.me>
2023-05-17 14:22:29 +0200

    Comm Working

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,23 @@ DBot *robot;
 void setup()
 {
   robot = new DBot();
-  robot->communication->setPin("0001");
-  robot->communication->startBluetooth();
-  // robot->communication->startWifiManager();
-  // robot->communication->startUDPServer(80);
-  robot->light->blink(12, 200);
+  robot->light->blink(4, 2000);
   robot->log("Robot initialise");
+  robot->log("UID: ");
+  robot->log(robot->communication->getID());
+  robot->communication->setPin("0001");
+  robot->communication->setName("DBot");
+  // robot->communication->startBluetooth();
+  robot->communication->startWifiManager();
+  robot->communication->startUDPServer(80);
 }
 
 void loop()
 {
   if (robot->communication->bluetoothAvailable())
   {
-    robot->light->up(12, 50);
     robot->log("New message via Bluetooth");
+    robot->log(robot->communication->getBluetoothData());
     robot->communication->bluetoothSend("Message recu 5 sur 5 mon géneral!");
   }
 }
